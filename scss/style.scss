6bf312a3ddb2e1439670fb76712c77cc7c212154
Jefferson <jefergiral@gmail.com>
2021-03-05 16:08:04 -0500

    Se agrega todos los estilos de la aplicación web

---
+++ scss/style.scss
@@ -0,0 +1,94 @@
+// Fuentes
+@font-face {
+  font-family: "Pokemon-Solid";
+  font-style: normal;
+  font-weight: normal;
+  src: url("/pokemon/fonts/Pokemon-Solid.woff") format("woff"), url("/pokemon/fonts/Pokemon-Solid.ttf") format("truetype");
+}
+
+@import url('https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap');
+
+
+.body {
+  width: 100%;
+  background-image: url(../assets/wallpaper-1.jpg);
+  // background-repeat: no-repeat;
+  background-size: cover;
+  transition: all 1s ease;
+  .header {
+    width: 100%;
+    background-color: rgb(182, 15, 15);
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    .box-img-pokeball {
+      width: 40px;
+      img {
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+    .box-img-text-pokemon {
+      width:75px;
+      img {
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+  .section-generate-pokemons {
+    margin-top: 125px;
+    text-align: center;
+    .btn {
+      padding: 10px 20px;
+      background-color: rgb(182, 15, 15);
+      color: #ffffff;
+      font-family: "Pokemon-Solid";
+      font-size: 30px;
+      letter-spacing: 3px;
+      border: none;
+      border-radius: 40px;
+      padding-bottom: 20px;
+      cursor: pointer;
+      margin-bottom: 30px;
+      outline: none;
+    }
+    .btnClear {
+      display: block;
+      margin: 0 auto;
+      font-size: 15px;
+      margin-bottom: 50px;
+    }
+    .ctnPokemons {
+      width: 100%;
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: space-evenly;
+    }
+  }
+}
+
+// Estilos a a caja del pokemon
+.boxPokemon {
+  width: 30%;
+  background-color:  rgba(248, 248, 248, 0.65);
+  border: 1px solid rgba(231, 228, 228, 0.75);
+  border-radius: 20px;
+  margin-bottom: 20px;
+  box-shadow: inset 0px 3px 1px 0 #000, 
+            inset 0 6px 1px 0px #ccc,
+            inset 0 9px 1.5px 0px #fff;
+}
+
+// Estilos al titulo
+.titlePokemon {
+  font-family: 'RocknRoll One', sans-serif;
+  font-size: 25px;
+  text-transform: uppercase;
+}
+
+// Estilos a la imagen del pokemon
+.imgPokemon {
+  width: 100%;
+  object-fit: cover;
+}
